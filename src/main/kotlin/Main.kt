074c87d00cd8505fff827742b85d6e0953088dc1
Kirill I. A <ghztrikz1337@gmail.com>
2023-04-12 12:03:40 +0300

    commit best solution as Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,6 @@
 import java.lang.System.currentTimeMillis
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import java.net.HttpURLConnection
+import java.net.URL
 
 const val PIPE_MIN_VALUE = 1
 const val PIPE_MAX_VALUE = 10
@@ -41,38 +39,10 @@ enum class Modifiers(val cost: Int) {
 
 enum class Method { GET, PUT, POST }
 
-fun HttpClient.send(request: HttpRequest) =
-    send(request, HttpResponse.BodyHandlers.ofString())
-
-fun dropRestrictions() {
-    System.setProperty("jdk.httpclient.allowRestrictedHeaders", "Referer")
-    System.setProperty("jdk.httpclient.redirects.retrylimit", "3")
-    System.setProperty("jdk.httpclient.disableRetryConnect", "true")
-    System.setProperty("jdk.httpclient.enableAllMethodRetry", "true")
-    System.setProperty("jdk.httpclient.allowRestrictedHeaders", "Content-Length")
-    System.setProperty("jdk.httpclient.allowRestrictedHeaders", "Host")
-    System.setProperty("jdk.httpclient.allowRestrictedHeaders", "Keep-Alive")
-    System.setProperty("jdk.httpclient.allowRestrictedHeaders", "Connection")
-    System.setProperty("jdk.internal.httpclient.disableHostnameVerification", "true")
-    System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true")
-    System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow", "|{}")
-}
-
 fun main(args: Array<String>) {
-    dropRestrictions()
     val host = args[0]
     val apiUrl = "http://$host/api"
     val token = args[1]
-
-    val client = HttpClient.newBuilder()
-        .followRedirects(HttpClient.Redirect.ALWAYS)
-        .version(HttpClient.Version.HTTP_1_1)
-        .build()
-
-    val baseRequest = HttpRequest.newBuilder()
-        .setHeader("Connection", "keep-alive")
-        .setHeader("Keep-Alive", "true")
-        .setHeader("Authorization", "Bearer $token")
 
     val robot = Robot()
     val observedPipes = listOf(Pipe(1), Pipe(2), Pipe(3))
@@ -84,77 +54,77 @@ fun main(args: Array<String>) {
         type: Modifiers? = null
     ) {
         val t1 = currentTimeMillis()
+        with(URL(url).openConnection() as HttpURLConnection) {
+            requestMethod = method.name
+            setRequestProperty("Authorization", "Bearer $token")
+            doOutput = true
 
-        val request = when (method) {
-            Method.GET -> baseRequest.uri(URI(url)).GET().build()
-            Method.PUT -> baseRequest.uri(URI(url)).PUT(HttpRequest.BodyPublishers.noBody()).build()
-            Method.POST -> baseRequest.setHeader("Content-Type", "application/json").uri(URI(url))
-                .POST(HttpRequest.BodyPublishers.ofString("""{"type":"${type!!.name}"}""")).build()
-        }
-
-        println("Request headers: ${request.headers()}")
-        val response = client.send(request)
-        println("Response headers: ${response.headers()}")
-        val responseCode = response.statusCode()
-
-        if (method == Method.POST) when (responseCode) {
-            200 -> {
-                robot.resources -= type!!.cost
-                println("apply modifier $type to pipe with url: $url")
+            if (type != null) {
+                setRequestProperty("Content-Type", "application/json")
+                outputStream.bufferedWriter().use { it.write("""{"type":"${type.name}"}""") }
             }
 
-            422 -> println("failed to apply modifier")
-            else -> println("$method: unexpected status code $responseCode")
-        }
-        else {
-            val newValue: Int
-            when (responseCode) {
-                200 -> newValue = PATTERN.find(response.body())?.value?.toInt() ?: 0
-
-                else -> {
-                    println("$method: unexpected status code $responseCode")
-                    return
+            if (method == Method.POST) when (responseCode) {
+                200 -> {
+                    robot.resources -= type!!.cost
+                    println("apply modifier $type to pipe with url: $url")
                 }
+
+                422 -> println("failed to apply modifier")
+                else -> println("$requestMethod: unexpected status code $responseCode")
             }
-
-            if (value != 0) {
-                if (newValue - value <= 4)
-                    direction = if (newValue > value) Direction.Up else Direction.Down
-
-                peoplesOnPipe = when (direction) {
-                    Direction.Down -> when (val peoplesOnPipe = value - newValue) {
-                        -9 -> 1
-                        -8 -> 2
-                        -7 -> 3
-                        -6 -> 4
-                        else -> peoplesOnPipe
+            else {
+                val newValue: Int
+                when (responseCode) {
+                    200 -> inputStream.bufferedReader().use {
+                        newValue = PATTERN.find(it.readText())?.value?.toInt() ?: 0
                     }
 
-                    else -> when (val peoplesOnPipe = newValue - value) {
-                        -9 -> 1
-                        -8 -> 2
-                        -7 -> 3
-                        -6 -> 4
-                        else -> peoplesOnPipe
+                    else -> {
+                        println("$requestMethod: unexpected status code $responseCode")
+                        return
                     }
                 }
 
-                if (peoplesOnPipe > 3) timeAllPeopleOnPipe = robot.gameTime
+                if (value != 0) {
+                    if (newValue - value <= 4)
+                        direction = if (newValue > value) Direction.Up else Direction.Down
+
+                    peoplesOnPipe = when (direction) {
+                        Direction.Down -> when (val peoplesOnPipe = value - newValue) {
+                            -9 -> 1
+                            -8 -> 2
+                            -7 -> 3
+                            -6 -> 4
+                            else -> peoplesOnPipe
+                        }
+
+                        else -> when (val peoplesOnPipe = newValue - value) {
+                            -9 -> 1
+                            -8 -> 2
+                            -7 -> 3
+                            -6 -> 4
+                            else -> peoplesOnPipe
+                        }
+                    }
+
+                    if (peoplesOnPipe > 3) timeAllPeopleOnPipe = robot.gameTime
+                }
+                value = newValue
+
+                if (method == Method.PUT) {
+                    robot.resources += value
+
+                    delay = (currentTimeMillis() - t1).toInt()
+                    robot.gameTime += delay
+                    timeOfCollectedValue = robot.gameTime
+                }
+
+                println(
+                    "Time: ${robot.gameTime}. Pipe: $number, peoples: $peoplesOnPipe. " +
+                            "Collected $value in $delay (total: ${robot.resources})"
+                )
             }
-
-            value = newValue
-            delay = (currentTimeMillis() - t1).toInt()
-
-            if (method == Method.PUT) {
-                robot.resources += value
-                robot.gameTime += delay
-                timeOfCollectedValue = robot.gameTime
-            }
-
-            println(
-                "Time: ${robot.gameTime}. Pipe: $number, peoples: $peoplesOnPipe. " +
-                        "Collected $value in $delay (total: ${robot.resources})"
-            )
         }
     }
 
@@ -178,24 +148,22 @@ fun main(args: Array<String>) {
     )
 
     fun Pipe.recalculateOutputValue() {
-        val valueTickTimes = (robot.gameTime - timeOfCollectedValue + delay) / delay
+        val valueTickTimes = (robot.gameTime - timeOfCollectedValue) / delay
         if (valueTickTimes == 0) return
 
         timeOfCollectedValue += delay * valueTickTimes
 
         var nextValue = value
-        val peoplesOnPipe = if (this == robot.bestPipe) this.peoplesOnPipe else this.peoplesOnPipe - 1
-
         when (direction) {
             Direction.Down -> for (tick in 0 until valueTickTimes) {
-                for (j in 0 until peoplesOnPipe) {
+                for (j in 0 until peoplesOnPipe - 1) {
                     nextValue--
                     if (nextValue < 1) nextValue = PIPE_MAX_VALUE
                 }
             }
 
             else -> for (tick in 0 until valueTickTimes) {
-                for (j in 0 until peoplesOnPipe) {
+                for (j in 0 until peoplesOnPipe - 1) {
                     nextValue++
                     if (nextValue > 10) nextValue = PIPE_MIN_VALUE
                 }
@@ -210,7 +178,7 @@ fun main(args: Array<String>) {
 
         recalculateOutputValue()
         when {
-            delay <= SIT_DELAY || (TIME_TO_DETERMINE_PIPE_VALUE / delay) * value >= 63 -> collect()
+            delay <= SIT_DELAY || (TIME_TO_DETERMINE_PIPE_VALUE / delay) * value >= 69 -> collect()
             else -> value()
         }
     }
@@ -225,6 +193,8 @@ fun main(args: Array<String>) {
         if (it.delay <= SIT_DELAY) return
     }
 
+    // todo: not lose points on it, stay tune and just collect and observe by findPipeWithPeoples()
+    // todo: deprecate count bad delay, we always on best, if best is bad, all other also bad
     fun ifAllPipesBadShuffleBaddestPipe() {
         var badDelay = 0
         observedPipes.forEach { if (it.delay >= BAD_DELAY) badDelay++ }
@@ -234,20 +204,18 @@ fun main(args: Array<String>) {
         }
     }
 
-    // 1 MS MAX
     fun findBestPipe() {
         var bestPipeValue = 0
+
         observedPipes.forEach {
             it.recalculateOutputValue()
 
             var localPipeValue = 0
             var localValue = it.value
 
-            val peoplesOnPipe = if (it == robot.bestPipe) it.peoplesOnPipe else it.peoplesOnPipe - 1
-
             when (it.direction) {
                 Direction.Down -> for (i in 0 until TIME_TO_DETERMINE_PIPE_VALUE step it.delay) {
-                    for (j in 0 until peoplesOnPipe) {
+                    for (j in 0 until it.peoplesOnPipe - 1) {
                         localValue--
                         if (localValue < 1) localValue = PIPE_MAX_VALUE
                     }
@@ -255,7 +223,7 @@ fun main(args: Array<String>) {
                 }
 
                 else -> for (i in 0 until TIME_TO_DETERMINE_PIPE_VALUE step it.delay) {
-                    for (j in 0 until peoplesOnPipe) {
+                    for (j in 0 until it.peoplesOnPipe - 1) {
                         localValue++
                         if (localValue > 10) localValue = PIPE_MIN_VALUE
                     }
@@ -291,7 +259,7 @@ fun main(args: Array<String>) {
         robot.bestPipe.collect()
 
         if (robot.gameTime >= 10000 &&
-            robot.bestPipe.delay >= SIT_DELAY + (robot.gameTime / PIPE_MEAN_DELAY) &&
+            robot.bestPipe.delay >= CHECK_DELAY + (robot.gameTime / PIPE_MEAN_DELAY) &&
             (robot.gameTime - robot.bestPipe.timeAllPeopleOnPipe) >= TIME_TO_THINK_BETTER_PIPE_EXISTS
         ) scanForPipes(exclude = robot.bestPipe)
 
